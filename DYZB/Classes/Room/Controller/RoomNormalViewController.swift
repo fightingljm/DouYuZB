eedbc8b08807cce1fbeb1bc25e0a6d551e6a5fd9
刘金萌 <1321839748ljm@gmail.com>
2019-08-15 09:26:57 +0800

    实现全屏Pop手势

--- DYZB/Classes/Room/Controller/RoomNormalViewController.swift
+++ DYZB/Classes/Room/Controller/RoomNormalViewController.swift
@@ -21,10 +21,6 @@ class RoomNormalViewController: UIViewController, UIGestureRecognizerDelegate {
         
         // 隐藏导航栏
         navigationController?.setNavigationBarHidden(true, animated: true)
-        
-        // 依然保持手势
-        navigationController?.interactivePopGestureRecognizer?.delegate = self
-        navigationController?.interactivePopGestureRecognizer?.isEnabled = true
     }
     
     
